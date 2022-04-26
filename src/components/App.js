fc2de352feac3027243de7b7bf7083a77e5134d7
Xusniddin <narkulof@gmail.com>
2022-04-27 01:02:51 +0500

    sdlksg

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,29 @@ import SearchBar from "./SearchBar.js";
 import ImageList from "./ImageList.js";
 
 class App extends React.Component {
-  state = { imageArr: [] };
+  constructor(props) {
+    super(props);
+    this.state = { imageArr: [], per: 10 };
+  }
 
   searchdanMalumotniOl = async (imgData) => {
     const data = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: imgData,
-        per_page: 10,
+        per_page: this.state.per,
       },
       headers: {
         Authorization: "Client-ID fFxqUKuJS8hx3nwzRQoYiHB5URcqFjkGG-FSsZUDOb4",
       },
     });
     this.setState({ imageArr: data.data.results });
+    console.log(data);
+  };
+
+  loadMore = async () => {
+    this.setState((old) => {
+      return { per: old.per + 10 };
+    });
   };
 
   render() {
@@ -24,7 +34,7 @@ class App extends React.Component {
       <div>
         <SearchBar malumotniOl={this.searchdanMalumotniOl} />
         <ImageList dataImg={this.state.imageArr} />
-        {/* <button>Loadmore</button> */}
+        <button onClick={this.loadMore}>Loadmore</button>
         {/* <div>{this.state.imageArr.length}</div> */}
       </div>
     );
